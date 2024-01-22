4390e7a8a98a0c033d4bb1553fa88b4b638b9c51
jgresham <johnsgresham@gmail.com>
2024-01-22 14:43:34 -0800

    watch index. tsnode

--- src/RedisClient.ts
+++ src/RedisClient.ts
@@ -1,12 +1,24 @@
 import { Redis } from '@upstash/redis'
+import 'dotenv/config'
 
 class RedisClient {
   private readonly client: Redis
 
   constructor() {
     // Replace these with your Upstash Redis connection details
-    const redisUrl = 'YOUR_UPSTASH_REDIS_URL'
-    const redisPassword = 'YOUR_UPSTASH_REDIS_PASSWORD'
+    if (
+      process.env.UPSTASH_REDIS_REST_URL === undefined ||
+      process.env.UPSTASH_REDIS_REST_TOKEN === undefined
+    ) {
+      throw new Error(
+        'env vars UPSTASH_REDIS_REST_URL and UPSTASH_REDIS_REST_TOKEN must be set.',
+      )
+    }
+    // } else {
+    //   console.log(process.env.UPSTASH_REDIS_REST_URL + " : " + process.env.UPSTASH_REDIS_REST_TOKEN);
+    // }
+    const redisUrl: string = process.env.UPSTASH_REDIS_REST_URL
+    const redisPassword: string = process.env.UPSTASH_REDIS_REST_TOKEN
 
     this.client = new Redis({
       url: redisUrl,
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import RedisClient from './RedisClient'
+
 console.log('Hello from TypeScript and Node.js!')
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const test = async () => {
+  const redisClient = new RedisClient()
+  const output = await redisClient.get('numActiveNodes')
+  console.log('numActiveNodes: ', output)
+}
+void test()
